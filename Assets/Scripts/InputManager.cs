fccce1fa5dda601cb5191168d4f44f364af3e38f
PankoU963 <alejandro_franco82211@elpoli.edu.co>
2025-10-04 20:58:13 -0500

    Add mouse input handling to InputManager
    InputManager now captures mouse X and Y input and stores it in mouseInput. The Update method passes mouseInput to the movement script, enabling mouse look functionality. Also adds a reference to the MouseLook script.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,10 +6,13 @@ public class InputManager : MonoBehaviour
 {
     [SerializeField] PlayerMovement movement;
 
+    [SerializeField] MouseLook mouseLook; // Reference to the MouseLook script
+
     private PlayerControls controls;
     private PlayerControls.GroundMovementActions groundMovement;
 
     [SerializeField] private Vector2 horizontalInput;
+    Vector2 mouseInput; //To store the input from the mouse
 
     private void Awake()
     {
@@ -21,10 +24,14 @@ public class InputManager : MonoBehaviour
         groundMovement.HorizontalMovement.performed += ctx => horizontalInput = ctx.ReadValue<Vector2>();
 
         groundMovement.Jump.performed += x => movement.OnJumpPressed();
+
+        groundMovement.MouseX.performed += ctx => mouseInput.x = ctx.ReadValue<float>();
+        groundMovement.MouseY.performed += ctx => mouseInput.y = ctx.ReadValue<float>();
     }
     
     private void Update() {
         movement.ReceiveInput(horizontalInput);
+        movement.ReceiveInput(mouseInput); //pass "mouseInput" to "ReceiveInput" method on "mouseLook" script
     }
     
     private void OnEnable()
